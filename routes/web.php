1c009b4892f37fa16be40f019a81068e1aa6a65c
LAPTOP-LCL1VDNQ\user <theekshanajayarathne@gmail.com>
2021-06-16 23:18:27 +0530

    apdate user

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });*/
+Auth::routes(['verify'=> true]);
 
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
@@ -34,7 +35,6 @@ Route::get('/users/{id}', function ($id) {
 });
 
 
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
